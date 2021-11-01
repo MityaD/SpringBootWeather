06cc1b0ab82d30a881d447f490add8082d6c1d78
Dima Yakushuk <yakushyk.dmitrii@gmail.com>
2021-11-02 02:27:03 +0300

    Init

---
+++ src/main/java/com/yakut/springbootweather/SpringBootWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.yakut.springbootweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yakut/springbootweather/SpringBootWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakut.springbootweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
